df69450243fc45f8a301954e2ac2883bf187d909
CassielKC <71156097+CassielKC@users.noreply.github.com>
2020-10-13 16:31:20 -0500

    Add files via upload

--- prac_temp.ino
+++ prac_temp.ino
@@ -6,13 +6,13 @@ void setup()
 
 void loop() 
 {
-   float voltaje, temp=0;
+   int voltaje, temp=0;
    delay(400);
    voltaje=analogRead(A0);
    Serial.println(voltaje);
    temp = (voltaje*57)/1023;
-   //temp = temp/1023;
    Serial.print("La temperatura es de: ");
    Serial.println(temp);
+   Serial.print(" °C");
    
 }
